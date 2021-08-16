4676edad7ccdff7639ee5dc24e0dc324241cbb55
NacreousDawn596 <75474839+NacreousDawn596@users.noreply.github.com>
2021-08-16 02:06:17 +0100

    Update .wisp.sh

--- .wisp.sh
+++ .wisp.sh
@@ -1,4 +1,4 @@
 function wispammer {
-	sudo chmod +x ~/.local/share/NacreousDawn596/main.sh
-	sudo bash ~/.local/share/NacreousDawn596/main.sh
+	sudo chmod +x ~/.local/share/NacreousDawn596/WiSpammer/main.sh
+	sudo bash ~/.local/share/NacreousDawn596/WiSpammer/main.sh
 }
