240395fc4a96e4d33e4baeb18e2b43d2975ba5dd
NacreousDawn596 <75474839+NacreousDawn596@users.noreply.github.com>
2023-05-06 15:09:46 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,13 +4,13 @@ echo 'please wait a few seconds...'
 
 sleep 1
 
-sudo apt-get install -y mdk3 macchanger pwgen python3 curl wget cowsay figlet wireless_tools net-tools
+sudo apt-get install -y mdk3 macchanger pwgen python curl wget cowsay figlet wireless_tools net-tools
 
-sudo pacman -Syu macchanger pwgen python3 curl wget cowsay figlet wireless_tools net-tools
+sudo pacman -Syu macchanger pwgen python curl wget cowsay figlet wireless_tools net-tools
 
-sudo dnf install mdk3 macchanger pwgen python3 curl wget cowsay figlet wireless_tools net-tools
+sudo dnf install mdk3 macchanger pwgen python curl wget cowsay figlet wireless_tools net-tools
 
-sudo apk install mdk3 macchanger pwgen python3 curl wget cowsay figlet wireless_tools net-tools
+sudo apk install mdk3 macchanger pwgen python curl wget cowsay figlet wireless_tools net-tools
 
 #clear
 
